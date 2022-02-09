f65e50c3a941c5a6ba5065368e8970c6ec725c8c
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-02-09 14:51:47 -0500

    Update E1.7.cpp

--- E1.7.cpp
+++ E1.7.cpp
@@ -5,11 +5,14 @@ Instructor: Tong Yi
 Assignment: HW E1.7
 This program will make you input three names and will print them out on 3 separate lines.
 */
+
 #include<iostream>
 using namespace std;
 int main(){
     string name1, name2, name3; 
+    //Three separate strings indicating names
     cout << "List first name: "; 
+    //User input asking for three names
     cin >> name1;
     cout << "List second name: ";
     cin >> name2;
