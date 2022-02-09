7217bce7e7166b56f5320cf2c2a3ccb4a1b350de
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-02-08 22:05:21 -0500

    Add files via upload

---
+++ E1.7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+//input three names and will print those names out in 3 separate lines.
+using namespace std;
+
+int main()
+{
+    string name1, name2, name3;
+    cout << "List first name: ";
+    cin >> name1;
+    cout << "List second name: ";
+    cin >> name2;
+    cout << "List third name: ";
+    cin >> name3;
+    cout << name1 << endl;
+    cout << name2 << endl;
+    cout << name3 << endl;
+
+    return 0;
+}
