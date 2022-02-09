65b6179a3bfd612b66880367fffcd34d9772ad51
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-02-09 14:35:49 -0500

    Update E1.7.cpp

--- E1.7.cpp
+++ E1.7.cpp
@@ -1,11 +1,12 @@
-//input three names and will print those names out in 3 separate lines.
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    string name1, name2, name3;
-    cout << "List first name: ";
+/*
+Author: Sakib Khan
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: HW E1.7
+This program will make you input three names and will print them out on 3 separate lines.
+*/
+    string name1, name2, name3; 
+    cout << "List first name: "; //User input for three different names
     cin >> name1;
     cout << "List second name: ";
     cin >> name2;
